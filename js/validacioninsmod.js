f1cc0ff593bc585e08d70f69691c5c7ab0ed2031
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-02-17 17:51:56 +0100

    Validación ins / mod (1/2)

--- js/validacioninsmod.js
+++ js/validacioninsmod.js
@@ -1,1 +1,43 @@
 /* JavaScript de validaciones para modificacion.html / insert.html */
+
+function validaNombre() {
+    let nombre = document.getElementById("username");
+    let userHelp = document.getElementById("usernameHelp");
+    if (nombre.value == "" | nombre.value == null | nombre.value.replace(/\s/g, "") == "") {
+        userHelp.style.visibility="visible";
+        nombre.classList.add("form-novalidado");
+        return false;
+    }
+    nombre.classList.remove("form-novalidado");
+    userHelp.style.visibility="hidden";
+    return true;
+}
+
+function validaPasswords() {
+    let password = document.getElementById("passwordone");
+    let passwordHelp = document.getElementById("passwordHelp");
+    const patron = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    if (password.value == "" | password.value == null | password.value.replace(/\s/g, "") == "" | ! patron.test(password.value)) {
+        passwordHelp.style.visibility="visible";
+        password.classList.add("form-novalidado");
+        return false;
+    }
+    password.classList.remove("form-novalidado");
+    passwordHelp.style.visibility="hidden";
+    return true;
+}
+
+
+
+function validacion() {
+    if (validaNombre() & validaPasswords()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function borraErrores(id) {
+    document.getElementById(id).classList.remove("form-novalidado");
+    document.getElementById(id + "Help").style.visibility="hidden"
+}
